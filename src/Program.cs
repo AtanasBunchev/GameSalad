c354eadfcbd79f2aa4cc1a37f9ea74ced984b7eb
Atanas Bunchev <atanas.bunchev@gmail.com>
2023-12-08 06:14:05 +0200

    Program.cs: Add Services and Middlewares

--- src/Program.cs
+++ src/Program.cs
@@ -5,9 +5,26 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllersWithViews();
+
         var app = builder.Build();
 
-        app.MapGet("/", () => "Hello World!");
+        if(!app.Environment.IsDevelopment())
+        {
+            // app.UseExceptionHandler("/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseRouting();
+
+        // app.UseAuthentication();
+        // app.UseAuthorization();
+        // app.UseSession();
+
+        app.MapDefaultControllerRoute();
 
         app.Run();
     }
