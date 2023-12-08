ca8638a759c2910fa9f069a0c8a9abb3b2ea0e16
Atanas Bunchev <atanas.bunchev@gmail.com>
2023-12-08 18:07:18 +0200

    Add ViewModels.User.LoginVM

---
+++ src/ViewModels/User/LoginVM.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace GameSalad.ViewModels.User;
+
+public class LoginVM
+{
+    [DisplayName("Username: ")]
+    [Required(ErrorMessage = "*This field is Required!")]
+    public string? Username { get; set; }
+
+    [DisplayName("Password: ")]
+    [Required(ErrorMessage = "*This field is Required!")]
+    public string? Password { get; set; }
+}
