f5a01212890ee3eb44542ce9c8f57e775b0ea97c
Atanas Bunchev <atanas.bunchev@gmail.com>
2023-12-12 21:56:23 +0200

    Add TicTacToe interface

--- src/Controllers/GameController.cs
+++ src/Controllers/GameController.cs
@@ -1,4 +1,5 @@
 using GameSalad.Entities;
+using GameSalad.Games;
 using GameSalad.Repositories;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
@@ -22,9 +23,57 @@ namespace GameSalad.Controllers
             return RedirectToAction("Games", "Home");
         }
 
-        public IActionResult TicTacToe()
+        public IActionResult TicTacToe(string move)
         {
-            return View();
+            TicTacToe game = new TicTacToe();
+            ProcessGame(game, move);
+            if(move == null)
+                return View(game);
+            return RedirectToAction("TicTacToe");
+        }
+
+        protected void ProcessGame(IGame game, string? action = null)
+        {
+            var user = GetLoggedUser();
+            if(user == null)
+                return;
+
+            context.Entry(user)
+                .Collection(u => u.Games)
+                .Load();
+
+            GameEntry? entry = user.Games
+                .Where(g => g.Type == "TicTacToe" && g.Active == true)
+                .FirstOrDefault();
+
+            if(entry == null)
+            {
+                entry = new GameEntry
+                {
+                    Type = game.Type,
+                    UserId = user.Id,
+                    Active = !game.IsFinished(),
+                    Data = game.SaveState()
+                };
+                context.Games.Add(entry);
+                context.SaveChanges();
+            }
+            else if(entry.Data != null)
+            {
+                game.LoadState(entry.Data);
+            }
+
+            if(action != null)
+            {
+                var validActions = game.GetValidActions();
+                if(validActions.Contains(action))
+                {
+                    game.Action(action);
+                    entry.Active = !game.IsFinished();
+                    entry.Data = game.SaveState();
+                    context.SaveChanges();
+                }
+            }
         }
     }
 }
