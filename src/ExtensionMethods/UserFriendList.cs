cb84edb6415fe514abb0aee811b1fdb027fb6969
Atanas Bunchev <atanas.bunchev@gmail.com>
2023-12-12 13:58:50 +0200

    Implement UserFriendList.Follow

--- src/ExtensionMethods/UserFriendList.cs
+++ src/ExtensionMethods/UserFriendList.cs
@@ -5,9 +5,20 @@ namespace GameSalad.ExtensionMethods;
 
 public static class UserFriendList
 {
+    // TODO decide on argument order
+    // It's quite the dilema as i prefer ctx to be first.
+    // But then user1.follow(ctx, user2) reads worse than the opposite...
+    // user1 in context follows user2 / user1 follows user2 in contex
+
     public static void Follow(this User u, UsersDbContext ctx, User other)
     {
-        ;
+        var item = new UserFollowEntry
+        {
+            Follower = u,
+            Target = other
+        };
+        ctx.Add(item);
+        ctx.SaveChanges();
     }
 }
 
